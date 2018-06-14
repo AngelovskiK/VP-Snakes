95abb49641dc934b076f2bbb7a27c997164ddb8b
AquaSolid <filip.markoski45@gmail.com>
2018-06-14 19:08:10 +0200

    [A* still doesn't go up] Developed OOP Architecture: Added Board class, Menu class, Snake class

---
+++ board.js
@@ -0,0 +1,130 @@
+var Board = /** @class */ (function () {
+    function Board(container, width, height) {
+        this.container = container;
+        this.width = width;
+        this.height = height;
+        this.block_size = 80;
+        console.log(this.common_divisors(width, height));
+        var block_size_float = parseFloat(this.block_size.toString());
+        this.ten_percent = (block_size_float * 0.01);
+        console.log(this.ten_percent);
+        this.hSize = this.width / this.block_size;
+        this.vSize = this.height / this.block_size;
+        this.menu = new Menu();
+        this.traversal = new Traversal();
+        this.FOOD_COLOR = "red";
+        this.food_exists = false;
+        this.food_point = new Point(0, 0);
+    }
+    Board.prototype.gcd = function (a, b) {
+        return (b === 0) ? a : this.gcd(b, a % b);
+    };
+    Board.prototype.common_divisors = function (a, b) {
+        var result = [];
+        var min = Math.min(a, b);
+        for (var i = 1; i < min; i += 1) {
+            if (a % i === 0 && b % i === 0) {
+                result.push(i);
+            }
+        }
+        return result;
+    };
+    Board.prototype.LCM = function (A) {
+        var n = A.length, a = Math.abs(A[0]);
+        for (var i = 1; i < n; i++) {
+            var b = Math.abs(A[i]), c = a;
+            while (a && b) {
+                a > b ? a %= b : b %= a;
+            }
+            a = Math.abs(c * A[i]) / (a + b);
+        }
+        return a;
+    };
+    Board.prototype.start = function () {
+        var _this = this;
+        this.erase();
+        this.addBackToMenuButton();
+        var ctx = this.canvas.getContext("2d");
+        ctx.font = "22px Arial";
+        this.draw(ctx);
+        var snake = new Snake(2, 2, Direction.Right, "blue");
+        var interval = 60 * 3;
+        // this uses a lambda wrapper function
+        var cycle = setInterval(function () { return _this.animate(ctx, snake, interval); }, interval);
+    };
+    Board.prototype.animate = function (ctx, snake, interval) {
+        this.draw(ctx);
+        if (!Point.isInList(snake.head, snake.trail)) {
+            snake.trail.push(snake.head);
+            while (snake.trail.length > snake.length) {
+                snake.trail.shift();
+            }
+            snake.draw(ctx, this.block_size, this.ten_percent, this.vSize, this.hSize);
+            this.drawFood(ctx, this.food_point, this.FOOD_COLOR);
+            if (snake.trail.length === snake.length) {
+                snake.head = snake.get_next_move(this.food_point, this.vSize, this.hSize);
+            }
+            else {
+                snake.head = new Point(snake.head.x + 1, snake.head.y);
+            }
+            if (snake.head.equals(this.food_point)) {
+                snake.length += 1;
+                this.food_point = this.getRandomPointNotInList(snake.trail);
+            }
+        }
+        ctx.fillText("Player 1 - Length: " + snake.length + " Speed: " + interval, this.block_size, this.block_size);
+    };
+    Board.prototype.draw = function (ctx) {
+        ctx.fillStyle = 'black';
+        ctx.fillRect(0, 0, WIDTH, HEIGHT);
+        for (var i = 0; i < HEIGHT; i++) {
+            for (var j = 0; j < WIDTH; j++) {
+                if (i === 0 && j === 0) {
+                    ctx.fillStyle = "red";
+                    ctx.fillRect(i * this.block_size, j * this.block_size, this.block_size, this.block_size);
+                }
+                else {
+                    ctx.fillStyle = "gray";
+                    ctx.fillRect(i * this.block_size, j * this.block_size, this.block_size, this.block_size);
+                }
+                ctx.fillStyle = "white";
+                ctx.fillRect(i * this.block_size + this.ten_percent, j * this.block_size + this.ten_percent, this.block_size - 2 * this.ten_percent, this.block_size - 2 * this.ten_percent);
+            }
+        }
+    };
+    Board.prototype.drawFood = function (ctx, point, color) {
+        ctx.fillStyle = color;
+        ctx.fillRect(point.x * this.block_size + this.ten_percent, point.y * this.block_size + this.ten_percent, this.block_size - 2 * this.ten_percent, this.block_size - 2 * this.ten_percent);
+    };
+    Board.prototype.getRandomPointNotInList = function (obstacles) {
+        // create an apple on a random tile until it doesn't overlap player
+        var x, y;
+        var point = null;
+        do {
+            x = Math.floor(Math.random() * hSize);
+            y = Math.floor(Math.random() * vSize);
+            point = new Point(x, y);
+        } while (Point.isInList(point, obstacles));
+        return point;
+    };
+    Board.prototype.erase = function () {
+        this.container.innerHTML = "";
+        var canvas = document.createElement("canvas");
+        canvas.setAttribute('width', this.width.toString());
+        canvas.setAttribute('height', this.height.toString());
+        canvas.id = 'game';
+        this.canvas = canvas;
+        this.container.appendChild(canvas);
+    };
+    Board.prototype.addBackToMenuButton = function () {
+        var _this = this;
+        var quitButton = document.createElement("button");
+        quitButton.className = 'quit';
+        quitButton.appendChild(document.createTextNode("✖"));
+        quitButton.addEventListener("click", function () { return _this.menu.ShowMenu(_this.container); });
+        this.btnQuit = quitButton;
+        this.container.appendChild(quitButton);
+    };
+    return Board;
+}());
+//# sourceMappingURL=board.js.map
---
+++ snake.js
@@ -0,0 +1,53 @@
+var Snake = /** @class */ (function () {
+    // food: Array<Point>, obstacles: Array<Point>
+    function Snake(x, y, direction, color) {
+        this.traversal = new Traversal();
+        this.head = new Point(x, y);
+        this.direction = direction;
+        this.color = color;
+        this.length = 3;
+        this.trail = [];
+    }
+    Snake.prototype.get_next_move = function (food_point, vSize, hSize) {
+        var problem = new Problem(this.head, this.direction, [food_point], this.trail, vSize, hSize);
+        var answer = this.traversal.astar_graph_search(problem);
+        var solution = answer.solution();
+        var solve = answer.solve();
+        var playerMovements = solve.map(function (state) { return state[0]; });
+        console.log(solution);
+        console.log(solve);
+        console.log(playerMovements);
+        console.log("trail: ", this.trail);
+        console.log("snake.head", this.head);
+        return solve[1][0];
+    };
+    Snake.prototype.draw = function (ctx, block_size, ten_percent, vSize, hSize) {
+        ctx.fillStyle = this.color;
+        this.trail.forEach(function (block) {
+            return ctx.fillRect(block.x * block_size, block.y * block_size, block_size - ten_percent * 4, block_size - ten_percent * 4);
+        });
+    };
+    Snake.prototype.move = function (direction) {
+        switch (direction) {
+            case Direction.Up: {
+                this.head = new Point(this.head.x - 1, this.head.y);
+                return this.head;
+            }
+            case Direction.Down: {
+                this.head = new Point(this.head.x + 1, this.head.y);
+                return this.head;
+            }
+            case Direction.Left: {
+                this.head = new Point(this.head.x, this.head.y + 1);
+                return this.head;
+            }
+            case Direction.Right: {
+                this.head = new Point(this.head.x, this.head.y - 1);
+                return this.head;
+            }
+        }
+        return null;
+    };
+    return Snake;
+}());
+//# sourceMappingURL=snake.js.map
